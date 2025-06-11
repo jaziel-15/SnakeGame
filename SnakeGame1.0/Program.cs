72e8f774022f2063431ac3d47061ee78761d1fbf
jaziel-15 <jaziel15gamer@gmail.com>
2025-06-11 14:24:18 -0600

    Se agregan las variable globales para el funcionamiento del juego

---
+++ SnakeGame1.0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace SnakeGame1._0
+{
+    internal class Program
+    {
+        static int weight = 20;
+        static int height = 20;
+        static int score = 0;
+        static bool gameOver = false;
+        static Random random = new Random();
+
+        static int snakeX;
+        static int snakeY;
+        static int fruitX;
+        static int fruitY;
+        static int[] tailX = new int[100];
+        static int[] tailY = new int[100];
+        static int taillenght = 0;
+        static int speed = 10;
+        static int direction = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
